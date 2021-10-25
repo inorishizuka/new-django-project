d615fac543da1519077949a067c4d031858480bd
Dewansh Khandelwal <dewansh3255@gmail.com>
2021-10-25 14:05:55 +0530

    Some important changes
    I made these changes so that some errors could be removes

--- scripts/HelloJavaN.java
+++ scripts/HelloJavaN.java
@@ -9,3 +9,22 @@ public class HelloJavaN {
         System.out.println("Hello, world!");
     }
 }
+
+class HelloWorld
+{
+  public static void main(String{} args)
+  {
+    System.out.println("This is a non-public class so that we could print the Hello world message");
+    System.out.println(10+20);
+    System.out.println(10-20);   
+    System.out.println(10-20);   
+    System.out.println(10/20);   
+  }
+}
+
+class extra
+{
+  int a = 20;
+  int b = 30;
+  System.out.println("The sum of A and B = "+(a+b));
+}
